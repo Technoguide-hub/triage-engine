71c249a98131ab710ac21c25a5ecebab7232923c
Matheus Nunes <math.nuunes@gmail.com>
2026-02-17 00:37:33 -0300

    get auth me

--- app/auth/deps.py
+++ app/auth/deps.py
@@ -13,20 +13,18 @@ def get_current_user(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme),
 ) -> User:
+
     try:
         payload = decode_token(token)
-        email = payload.get("sub")
+        user_id = payload.get("sub")
 
-        if not email:
+        if not user_id:
             raise ValueError("Invalid token payload")
 
     except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-        )
+        raise HTTPException(status_code=401, detail="Invalid token")
 
-    user = db.query(User).filter(User.email == email).first()
+    user = db.query(User).filter(User.id == user_id).first()
 
     if not user:
         raise HTTPException(status_code=401, detail="User not found")
